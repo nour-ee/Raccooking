844bed22a68fc971df6c5cc267929ec82fac4e96
victoria.myot <victoria.myot@universite-paris-saclay.fr>
2025-03-10 14:54:39 +0100

    Baker.buy()

--- Model/Baker.java
+++ Model/Baker.java
@@ -1,19 +1,42 @@
 package Model;
 
 /**
- * Classe Joueur : à voir si elle hérite d'une classe Entité ou pas
- * Le joueur connait sa position actuelle, il est posé sur une carte
+ * Baker : our player class !
  */
 public class Baker extends Entity {
+
+    /****************
+     *  ATTRIBUTES  *
+     ****************/
     private int money; // current money of the player
-    //TO DO : array with ingredients inventory
-    private int[] ressources; //0 flour, 1 egg, 2 yeast, 3 butter
+    private int[] ressources; //Stores the ressources of the baker
+                              // in the following indexes:
+                              // 0 flour, 1 egg, 2 yeast, 3 butter
     private int soldBread; // number of breads sold
 
+    /****************
+     *    GETTERS   *
+     ****************/
+    public Tile getPosition() { return position; }
+    public int getMoney() { return money; }
+
+    /********************
+     *    CONSTRUCTOR   *
+     ********************/
     public Baker(Tile c){
         this.position = c; this.money = 0;
+        this.ressources = new int[4];
+        this.soldBread = 0;
     }
 
+    /****************
+     *    METHODS   *
+     ****************/
+
+    /**
+     * Method to move the player
+     * @param c the tile to move to
+     */
     public void move(Tile c){
         if(c.isTraversable()){
             this.position = c;
@@ -26,6 +49,21 @@ public class Baker extends Entity {
         }
     }
 
-    public Tile getPosition() { return position; }
-    public int getMoney() { return money; }
+    /**
+     * Method to buy ressources
+     * @param s string representing the ressource to buy
+     *          "flour", "egg", "yeast", "butter"
+     */
+    public void buy(String s){
+        if( money >= 2 ) {
+            switch (s) {
+                case "flour" : ressources[0]++; money=money-2; break;
+                case "egg" : ressources[1]++; money=money-2; break;
+                case "yeast" : ressources[2]++; money=money-2; break;
+                case "butter" : ressources[3]++; money=money-2; break;
+            }
+        }
+    }
+
+
 }
