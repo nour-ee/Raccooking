db3ca7ba0480ec4ba336c1113aae4a2580cb4a33
Nour ettayeb <nour.ettayeb@universite-paris-saclay.fr>
2025-03-14 06:42:28 +0100

    organize code and add paint

--- View/Display.java
+++ View/Display.java
@@ -1,4 +1,5 @@
 package View;
+import Controller.BakerMovement;
 import Controller.EntityControl;
 import Model.*;
 
@@ -7,20 +8,31 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Display extends JFrame {
-    public static final int TILE_SIZE = 75;   //size of a tile, in pixels (square tile)
-    public static final int MARGIN = 10; //margin around the bakery, in pixels
+
+    /****************
+     *  ATTRIBUTES  *
+     ****************/
     private BakerPanel bkPanel;
     private RaccoonPanel rcPanel;
 
     private EntityControl entityControl;
+
+    private BakerMovement bakerMovement;
 
     private Bakery bakery ;
 
     private JLabel bakerLabel;
     private ArrayList<JLabel> raccoonLabels = new ArrayList<JLabel>();
     private ArrayList<RaccoonPanel> raccoonPanels = new ArrayList<RaccoonPanel>();
+
+    public static final int TILE_SIZE = 75;   //size of a tile, in pixels (square tile)
+    public static final int MARGIN = 10; //margin around the bakery, in pixels
     public static final int FRAME_H = Bakery.BAKERY_H*TILE_SIZE + 2*MARGIN;
     public static final int FRAME_W = Bakery.BAKERY_W*TILE_SIZE + 2*MARGIN;
+
+    /********************
+     *    CONSTRUCTOR   *
+     ********************/
     public Display() {
         setTitle("Raccooking");
         setSize(FRAME_W+BakerPanel.WIDTH, FRAME_H);
@@ -33,17 +45,8 @@ public class Display extends JFrame {
         // Add the Bakerpanel to the Bakery frame
         bkPanel = new BakerPanel(bakery);
         bkPanel.setVisible(false);
+        bakerMovement = new BakerMovement(bakery);
 
-
-        //Add the baker to the Bakery frame for testing
-        ImageIcon bakerIcon = new ImageIcon(getClass().getResource("/img/baker.png"));
-        int newWidth = TILE_SIZE;
-        int newHeight = TILE_SIZE;
-        Image scaledImage = bakerIcon.getImage().getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
-        ImageIcon scaledBakerIcon = new ImageIcon(scaledImage);
-        bakerLabel = new JLabel(scaledBakerIcon);
-        bakerLabel.setBounds(coord(bakery.getPlayer().getPosition().getX(), bakery.getPlayer().getPosition().getY()).x, coord(bakery.getPlayer().getPosition().getX(), bakery.getPlayer().getPosition().getY()).y, newWidth, newHeight);
-        add(bakerLabel);
 
         // Add the RaccoonPanel to the Bakery frame
         rcPanel = new RaccoonPanel();
@@ -52,38 +55,56 @@ public class Display extends JFrame {
         // Manage interaction with bakery elements
         entityControl = new EntityControl(rcPanel, bkPanel, this);
 
+        // Add the Baker to the Bakery frame
+        placeBaker();
         //Add raccoons to the Bakery frame
         placeRaccoons();
-
-
-        // bakerpanel appears on the right when I click on bakerlabel
-        bakerLabel.addMouseListener(entityControl);
-        placeBread();
+        //Add bread to the Bakery frame
+        //placeBread();
 
         //set frame visible
         setVisible(true);
     }
-    public RaccoonPanel getRaccoonPanel() {
-        return rcPanel;
-    }
 
-    public JLabel getBakerLabel() {
-        return bakerLabel;
-    }
+    /****************
+     *    GETTERS   *
+     ****************/
+    public RaccoonPanel getRaccoonPanel() {return rcPanel;}
+    public JLabel getBakerLabel() {return bakerLabel;}
+    public BakerPanel getBakerPanel() {return bkPanel;}
+    public ArrayList<JLabel> getRaccoonLabels() {return raccoonLabels;}
 
-
-    public BakerPanel getBakerPanel() {
-        return bkPanel;
-    }
-
-    public ArrayList<JLabel> getRaccoonLabels() {
-        return raccoonLabels;
-    }
+    /****************
+     *    METHODS   *
+     ****************/
 
     public Point coord(int x, int y) {
         return new Point(x*TILE_SIZE, y*TILE_SIZE);
 
     }
+    /**place the baker on the bakery by getting its position**/
+    private void placeBaker() {
+        Baker b = bakery.getPlayer();
+        int x = b.getPosition().getX();
+        int y = b.getPosition().getY();
+
+        ImageIcon bakerIcon = new ImageIcon(getClass().getResource("/img/baker.png"));
+        int newWidth = TILE_SIZE;
+        int newHeight = TILE_SIZE;
+        Image scaledImage = bakerIcon.getImage().getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        ImageIcon scaledBakerIcon = new ImageIcon(scaledImage);
+
+        bakerLabel = new JLabel(scaledBakerIcon);
+        bakerLabel.setBounds(coord(x, y).x, coord(x, y).y, newWidth, newHeight);
+
+        // bakerpanel appears on the right when I click on bakerlabel
+        bakerLabel.addMouseListener(entityControl);
+        bakerLabel.addKeyListener(bakerMovement);
+
+        add(bakerLabel);
+    }
+
+    /**place the raccoons on the bakery by getting their positions**/
     private void placeRaccoons() {
         Raccoon[] rc = bakery.getRaccoons();
         for (Raccoon r : rc) {
@@ -113,29 +134,40 @@ public class Display extends JFrame {
         }
     }
 
-        private void placeBread(){
-            Tile[][] map = bakery.getMap();
-            for (int i = 0; i < Bakery.BAKERY_H; i++) {
-                for (int j = 0; j < Bakery.BAKERY_W; j++) {
-                    if (map[i][j].hasOven()) {
-                        Oven o = (Oven) map[i][j];
-                        if (o.isOccupied()) {
-                            Bread b = o.getBread();
-                            if (true) { //il y avait b.isCooked() avant ici mais c'est pas très logique donc en attendant
-                                ImageIcon breadIcon = new ImageIcon(getClass().getResource("/img/bread.png"));
-                                int newWidth = TILE_SIZE;
-                                int newHeight = TILE_SIZE;
-                                Image scaledImage = breadIcon.getImage().getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
-                                ImageIcon scaledBreadIcon = new ImageIcon(scaledImage);
+    /**place the bread on the bakery by getting the ovens**/
+    private void placeBread(){
+        Tile[][] map = bakery.getMap();
+        for (int i = 0; i < Bakery.BAKERY_H; i++) {
+            for (int j = 0; j < Bakery.BAKERY_W; j++) {
+                if (map[i][j].hasOven()) {
+                    Oven o = (Oven) map[i][j];
+                    if (o.isOccupied()) {
+                        Bread b = o.getBread();
+                        if (true) { //il y avait b.isCooked() avant ici mais c'est pas très logique donc en attendant
+                            ImageIcon breadIcon = new ImageIcon(getClass().getResource("/img/bread.png"));
+                            int newWidth = TILE_SIZE;
+                            int newHeight = TILE_SIZE;
+                            Image scaledImage = breadIcon.getImage().getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+                            ImageIcon scaledBreadIcon = new ImageIcon(scaledImage);
 
-                                JLabel breadLabel = new JLabel(scaledBreadIcon);
-                                Point coord = coord(o.getX(),o.getY());
-                                breadLabel.setBounds(coord.x, coord.y, newWidth, newHeight);
-                                add(breadLabel);
-                            }
+                            JLabel breadLabel = new JLabel(scaledBreadIcon);
+                            Point coord = coord(o.getX(),o.getY());
+                            breadLabel.setBounds(coord.x, coord.y, newWidth, newHeight);
+                            add(breadLabel);
                         }
                     }
                 }
+            }
         }
+    }
+    public void paint(Graphics g) {
+        super.paint(g);
+        placeBread();
+        //update the baker position
+        int x = bakery.getPlayer().getPosition().getX();
+        int y = bakery.getPlayer().getPosition().getY();
+        bakerLabel.setBounds(coord(x, y).x, coord(x, y).y, TILE_SIZE, TILE_SIZE);//la position reste toujours à 0,0
+        placeRaccoons();
+
     }
 }
